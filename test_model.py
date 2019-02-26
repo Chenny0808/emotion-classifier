8e04fa9dc703dcf31b15e5c4d094ac36eabf1133
bigprince <zuofeng1997@gmail.com>
2019-02-26 11:45:32 +0800

    add test

--- test_model.py
+++ test_model.py
@@ -1,12 +1,13 @@
 import json
 import requests
 import numpy as np
+from rnn_model import my_tokenizer
+if __name__ == '__main__':
 
-# payload ={
-#     "instances": [{'input_sentence': [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}]
-# }
-#
-# print(requests.post('http://127.0.0.1:9000/v1/models/mymodel:predict', json=payload).content)
-f = open("train.tsv", 'r', encoding="utf-8")
-for i in f.readlines():
-    print(i.split("\t"))
+    sentences = my_tokenizer.texts_to_sequences([" ".join("今天下大雪，外卖小哥没有迟到，真的敬业，点个赞！")])
+
+    payload = {
+        "instances": [{'input_sentence': sentences[0]}]
+    }
+
+    print(requests.post('http://127.0.0.1:9000/v1/models/ec:predict', json=payload).content)
